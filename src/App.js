f22cb94071d4ba4901658079f48b62702a3d9e4c
TMBuddhika <buddhikawgt@gmail.com>
2025-02-28 18:17:17 +0530

    COMMIT 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import gptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import msgIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideBar">
+        <div className='upperSide'>
+          <div className='upperSideTop'>
+            <img src={gptLogo} alt="Logo" className="logo" /><span className='brand'>
+              ChatGPT</span></div>
+
+            <button className='midBtn'>
+              <img src={addBtn} alt="new Chat" className="addBtn"/>
+              New Chat
+            </button>
+            <div className="upperSideBottom">
+              <button className="query">
+                <img src ={msgIcon} alt="Query"/>What is Programming?  
+              </button>
+              <button className="query">
+                <img src ={msgIcon} alt="Query"/>How to use an API  
+              </button>
+            </div>
+          </div>
+
+          {/* Move lowerSide inside sideBar */}
+          <div className='lowerSide'>
+            <div className="listItems"><img src={home} alt="Home"/>Home</div>
+            <div className="listItems"><img src={saved} alt="Saved"/>Saved</div>
+            <div className="listItems"><img src={rocket} alt="Upgrade"/>Upgrade to Pro</div>
+          </div>
+      </div> 
+      
+
+      <div className='main'>
+      </div>
     </div>
   );
 }
 
 export default App;
+
